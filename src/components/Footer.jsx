05d94e52974e1252515c70b4f220fd22c9e2a38e
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-05 10:37:13 +0100

    Create footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
-import "../styles/Footer.css";
+import "../styles/footer.css";
 
 export default function Header() {
 	return (
 		<>
 			<footer className="footer">
-				<p></p>
+				<p>
+					CV App made with{" "}
+					<a href="https://react.dev/" target="_blank">
+						React
+					</a>{" "}
+					|{" "}
+					<a
+						href="https://github.com/joaquimlcbfranco/cv-app"
+						target="_blank"
+					>
+						Source Code
+					</a>{" "}
+					|{" "}
+					<a
+						href="https://github.com/joaquimlcbfranco"
+						target="_blank"
+					>
+						GitHub
+					</a>
+				</p>
 			</footer>
 		</>
 	);
