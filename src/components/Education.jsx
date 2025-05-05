ff82cce20f344c205abbe9a9bcf4997a712e99e5
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-05 13:06:00 +0100

    Add education and experience sections

---
+++ src/components/Education.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "../styles/education.css";
+
+function Education() {
+  const [education, setEducation] = useState({
+    school: "Lorem University",
+    title: "Bachelor's in Lorem",
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+
+  const handleChange = (e, id) => {
+    if (id === "school") {
+      setEducation({ ...education, school: e.target.value });
+    } else if (id === "title") {
+      setEducation({ ...education, title: e.target.value });
+    } else if (id === "startDate") {
+      setEducation({ ...education, startDate: e.target.value });
+    } else if (id === "endDate") {
+      setEducation({ ...education, endDate: e.target.value });
+    }
+  };
+
+  return (
+    <>
+      <section className="education">
+        <div className="education-inputs">
+          <h2>Education</h2>
+          <Input
+            label="School"
+            type="text"
+            id="school"
+            value={education.school}
+            onType={handleChange}
+          ></Input>
+          <Input
+            label="Title"
+            type="text"
+            id="title"
+            value={education.title}
+            onType={handleChange}
+          ></Input>
+          <Input
+            label="Start Date"
+            type="date"
+            id="startDate"
+            value={education.startDate}
+            onType={handleChange}
+          ></Input>
+          <Input
+            label="End Date"
+            type="date"
+            id="endDate"
+            value={education.endDate}
+            onType={handleChange}
+          ></Input>
+        </div>
+      </section>
+    </>
+  );
+}
+
+function Input({ label, type, id, value, onType }) {
+  return (
+    <label>
+      {label}
+      <input
+        type={type}
+        value={value}
+        onChange={(e) => onType(e, id)}
+      ></input>
+    </label>
+  );
+}
+
+export { Education };
---
+++ src/components/Experience.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "../styles/experience.css";
+
+function Experience() {
+	const [experience, setExperience] = useState({
+		company: "Lorem Company",
+		title: "Lorem Assistant",
+		startDate: new Date(),
+		endDate: new Date(),
+		location: "Lorem City",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus quam, sed voluptatibus debitis nisi blanditiis qui beatae ipsum libero.",
+	});
+
+	const handleChange = (e, id) => {
+		if (id === "school") {
+			setExperience({ ...experience, company: e.target.value });
+		} else if (id === "title") {
+			setExperience({ ...experience, title: e.target.value });
+		} else if (id === "startDate") {
+			setExperience({ ...experience, startDate: e.target.value });
+		} else if (id === "endDate") {
+			setExperience({ ...experience, endDate: e.target.value });
+		} else if (id === "location") {
+			setExperience({ ...experience, location: e.target.value });
+		} else if (id === "description") {
+			setExperience({ ...experience, description: e.target.value });
+		}
+	};
+
+	return (
+		<>
+			<section className="experience">
+				<div className="experience-inputs">
+					<h2>Experience</h2>
+					<Input
+						label="School"
+						type="text"
+						id="school"
+						value={experience.company}
+						onType={handleChange}
+					></Input>
+					<Input
+						label="Title"
+						type="text"
+						id="title"
+						value={experience.title}
+						onType={handleChange}
+					></Input>
+					<Input
+						label="Start Date"
+						type="date"
+						id="startDate"
+						value={experience.startDate}
+						onType={handleChange}
+					></Input>
+					<Input
+						label="End Date"
+						type="date"
+						id="endDate"
+						value={experience.endDate}
+						onType={handleChange}
+					></Input>
+					<Input
+						label="End Date"
+						type="date"
+						id="endDate"
+						value={experience.location}
+						onType={handleChange}
+					></Input>
+					<Input
+						label="End Date"
+						type="date"
+						id="endDate"
+						value={experience.description}
+						onType={handleChange}
+					></Input>
+				</div>
+			</section>
+		</>
+	);
+}
+
+function Input({ label, type, id, value, onType }) {
+	return (
+		<label>
+			{label}
+			<input
+				type={type}
+				value={value}
+				onChange={(e) => onType(e, id)}
+			></input>
+		</label>
+	);
+}
+
+export { Experience };
