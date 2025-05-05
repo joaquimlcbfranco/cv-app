4f7dc3c0daab435a5b300a615020b91e96128fc0
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-05 12:26:15 +0100

    fix: Style #root instead of body element

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import { Personal } from './components/Personal.jsx'
 import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Personal />
     <Footer />
   </StrictMode>,
 )
