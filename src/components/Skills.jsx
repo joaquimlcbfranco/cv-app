7e0a2e3abb638df9ce398b2f06c60b5a9db68e14
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-05-12 20:23:32 +0100

    feat: Change setSkills, so when new skill button is clicked it adds a new empty one to array

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,20 +2,33 @@ import "../styles/skills.css";
 
 export default function Skills({ skills, setSkills }) {
 	const handleChange = (e, id) => {
-    const newArray = skills.map(skill => {
-      return skill.id === id ? {...skill, value: e.target.value} : skill
-    });
-    setSkills(newArray);
+		const newArray = skills.map((skill) => {
+			return skill.id === id
+				? { ...skill, value: e.target.value }
+				: skill;
+		});
+		setSkills(newArray);
 	};
 
-  const skillsInputs = skills.map(skill => <Input key={skill.id} id={skill.id} type="text" value={skill.value} onType={handleChange}></Input>);
+  const handleNewSection = () => {
+    setSkills([...skills, ""]);
+  }
 
 	return (
 		<>
 			<section className="skills">
 				<div className="skills-inputs">
 					<h2>Skills</h2>
-          {skillsInputs}
+					{skills.map((skill) => (
+						<Input
+							key={skill.id}
+							id={skill.id}
+							type="text"
+							value={skill.value}
+							onType={handleChange}
+						></Input>
+					))}
+          <button onClick={handleNewSection}>Add Skill</button>
 				</div>
 			</section>
 		</>
@@ -24,11 +37,11 @@ export default function Skills({ skills, setSkills }) {
 
 function Input({ type, id, value, onType }) {
 	return (
-			<input
-				type={type}
-				value={value}
-        id={id}
-				onChange={(e) => onType(e, id)}
-			></input>
+		<input
+			type={type}
+			value={value}
+			id={id}
+			onChange={(e) => onType(e, id)}
+		></input>
 	);
 }
